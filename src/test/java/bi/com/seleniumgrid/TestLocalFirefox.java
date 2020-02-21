1e66e5b2f0847c77a5b05c0697e19ff2554fc8a5
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-21 11:28:32 -0500

    Update TestLocalFirefox.java

--- src/test/java/bi/com/seleniumgrid/TestLocalFirefox.java
+++ src/test/java/bi/com/seleniumgrid/TestLocalFirefox.java
@@ -30,8 +30,7 @@ public void verifyTitle()
 {
 driver.get("https://www.amazon.com");
 String ActualTitle = driver.getTitle();
-String ExpectedTitle = (“Amazon.com: Online Shopping for Electronics, Apparel,
-    Computers, Books, DVDs & more”);
+String ExpectedTitle = “Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more”;
 Assert.assertEquals(ActualTitle, ExpectedTitle);
 System.out.println(“Assert passed”);
 }
