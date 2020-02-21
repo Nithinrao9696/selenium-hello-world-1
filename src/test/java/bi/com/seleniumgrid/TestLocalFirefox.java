feb3848bd8e5e85730f49d74da4701dd3e7d1fb3
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-21 11:25:32 -0500

    Update TestLocalFirefox.java

--- src/test/java/bi/com/seleniumgrid/TestLocalFirefox.java
+++ src/test/java/bi/com/seleniumgrid/TestLocalFirefox.java
@@ -28,7 +28,7 @@ public class TestLocalFirefox {
  @Test
 public void verifyTitle()
 {
-driver.get(https://www.amazon.com);
+driver.get("https://www.amazon.com");
 String ActualTitle = driver.getTitle();
 String ExpectedTitle = “Amazon.com: Online Shopping for Electronics, Apparel,
     Computers, Books, DVDs & more”;
